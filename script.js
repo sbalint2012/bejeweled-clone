f62162855e3e0f747579992971a23c4041b69732
sbalint2012 <sipos.balint07@gmail.com>
2023-04-13 10:06:29 +0200

    checking valid step

--- script.js
+++ script.js
@@ -66,18 +66,73 @@ function move_gems(arr){
     var gem_1_class = "." + arr[0][0] + "." + arr[0][1] + "." + arr[0][2];
     var gem_2_class = "." + arr[1][0] + "." + arr[1][1] + "." + arr[1][2];
 
-    console.log(gem_1_class);
-    $('#game_area').find(gem_1_class).each(function (){
+    //console.log(gem_1_class);
+
+    if(check_step(arr) && check_dist(arr)){
+        $('#game_area').find(gem_1_class).each(function (){
             $(this).removeClass(arr[0][0]);
-        $(this).attr('class', arr[1][0] + " " + $(this).attr('class'));
+            $(this).attr('class', arr[1][0] + " " + $(this).attr('class'));
         });
-    $('#game_area').find(gem_2_class).each(function (){
-        $(this).removeClass(arr[1][0]);
-        $(this).attr('class', arr[0][0] + " " + $(this).attr('class'));
-    });
+
+        $('#game_area').find(gem_2_class).each(function (){
+            $(this).removeClass(arr[1][0]);
+            $(this).attr('class', arr[0][0] + " " + $(this).attr('class'));
+        });
+    }
+    check_dist(arr);
+
 }
 
-function check_gem(){
+//lepes ellenorzes, csak vizszintes vagy fuggoleges
+function check_step(arr){
+    if(arr[0][1] == arr[1][1] || arr[0][2] == arr[1][2]){
+        return true;
+    }
+    return false;
+}
+//lepes tavolsag ellenorzes
+function check_dist(arr){
+    var gem_1_col;
+    var gem_1_row;
+    var gem_2_col;
+    var gem_2_row;
+
+    if(arr[0][1].length == 8){
+        gem_1_col = arr[0][1].slice(7,8);
+    } else {
+        gem_1_col = arr[0][1].slice(7,9);
+    }
+    if(arr[1][1].length == 8){
+        gem_2_col = arr[1][1].slice(7,8);
+    } else {
+        gem_2_col = arr[1][1].slice(7,9);
+    }
+
+    if(arr[0][2].length == 5){
+        gem_1_row = arr[0][2].slice(4,5);
+    } else {
+        gem_1_row = arr[0][2].slice(4,6);
+    }
+    if(arr[1][2].length == 5){
+        gem_2_row = arr[1][2].slice(4,5);
+    } else {
+        gem_2_row = arr[1][2].slice(4,6);
+    }
+
+    var col_abs = Math.abs(gem_1_col - gem_2_col);
+    var row_abs = Math.abs(gem_1_row - gem_2_row);
+
+    //console.log("row: " + row_abs);
+    //console.log("col: " + col_abs);
+    //console.log("gem1: " + gem_1_col + ", " + gem_1_row);
+    //console.log("gem2: " + gem_2_col + ", " + gem_2_row);
+
+    if(col_abs == 0 && row_abs == 1){
+        return true;
+    } else if(col_abs == 1 && row_abs ==0){
+        return true;
+    }
+    return false;
 
 }
 
